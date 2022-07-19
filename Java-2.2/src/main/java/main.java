41849babe16d52451742a861481a8a1d2f3b4d94
Денис <den_asy@mail.ru>
2022-07-19 15:50:47 +0300

    Получи рубль за каждые 100

---
+++ Java-2.2/src/main/java/main.java
@@ -0,0 +1,19 @@
+
+public class main {
+    public static void main(String[] args) {
+        int balance = 100; //начальный баланс клиента
+        int add = 1300; //переменная для суммы пополнения
+        int newBalance; //переменная для итогового баланса
+        int cashback; //переменная для бонусов
+
+        if (add > 1000) {
+            cashback = add / 100;
+            newBalance = balance + add + cashback;
+        } else {
+            cashback = 0;
+            newBalance = balance + add;
+        }
+        System.out.println("Кэшбек:" + cashback);
+        System.out.println("Ваш баланс:" + newBalance);
+    }
+}
